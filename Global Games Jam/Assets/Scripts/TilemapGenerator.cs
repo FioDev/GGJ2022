c210984b3ce62064142c329535014f7c4c8f0167
FioDev <77973580+FioDev@users.noreply.github.com>
2022-01-28 23:48:03 +0000

    Gap validation
    its not functional
    + also added tile Generation
    + changed return type to 2d array

--- Global Games Jam/Assets/Scripts/TilemapGenerator.cs
+++ Global Games Jam/Assets/Scripts/TilemapGenerator.cs
@@ -43,24 +43,83 @@ public class TilemapGenerator : MonoBehaviour
         
     }
 
-    public List<TileType> GenerateTileRow(int width, float heightToGenerate)
+    //Width in tiles. Playerwidth in tiles (IN THE SAME SCALE AS WIDTH), height to generate as a float (Any scale you want)
+    public List<TileType>[] GenerateTileRow(int width, int playerWidth, float heightToGenerate)
     {
-        List<TileType> temp = new List<TileType>();
-
-        //Get a set of values from Proc Noise that represents tiles
-        foreach (float x in noiseGen.GetNoiseAtHeight(width, heightToGenerate))
+        List<TileType>[] temp = new List<TileType>[2];
+        
+        for (int layer = 2; layer > -1; layer--)
         {
-            //Control ground / air gaps
-            if (x > 0.75)
+            switch (layer)
             {
-                temp.Add(TileType.Ground);
-            } else
-            {
-                temp.Add(TileType.Air);
+                case 0:
+
+                    foreach (TileType x in temp[layer - 1])
+                    {
+                        temp[layer].Add(TileType.Air);
+                    }
+
+                    break;
+
+                case 1:
+                    
+                    //Check previous layer for ground below. Generate a random number between 1 and 20.
+                    //If you roll a nat 20, spawn a collectable over the ground that would be there
+                    foreach (TileType x in temp[layer - 1])
+                    {
+                        int y = Random.Range(1, 20);
+                        if (y == 20) //Roll a 20, get a collectable (if spawnable in current position (there is ground under you))
+                        {
+                            if (x == TileType.Ground)
+                            {
+                                temp[layer].Add(TileType.Collectable);
+                            } else
+                            {
+                                temp[layer].Add(TileType.Air);
+                            }
+                        } else 
+                        {
+                            temp[layer].Add(TileType.Air);
+                        }
+                    }
+
+                    break;
+
+                case 2:
+
+                    //Get a set of values from Proc Noise that represents tiles
+                    foreach (float x in noiseGen.GetNoiseAtHeight(width, heightToGenerate))
+                    {
+                        //Control ground / air gaps
+                        if (x > 0.75)
+                        {
+                            temp[layer].Add(TileType.Ground);
+                        }
+                        else
+                        {
+                            temp[layer].Add(TileType.Air);
+                        }
+                    }
+
+                    temp[layer] = ValidateGaps(temp[layer], playerWidth);
+                    
+                    break;
             }
         }
 
-
         return temp;
+    }
+
+    private List<TileType> ValidateGaps(List<TileType> x, int targetWidth)
+    {
+
+        //This currently does absolutely nothing
+        //it SHOULD go through x (which is a list of tile types along a row) and record the maximum gap
+        //If the maximum gap on that row is larger than targetWidth (the width of a player or otherwise minimum gap) just return x
+        //If its less, change the next few or previous few tile types to air until validation passes
+        //Possibly do this recursivley.
+        //It might work
+        //you never know
+        return x;
     }
 }
