21a323b7939be3eb5363de7e7938c60676dd23ed
FioDev <77973580+FioDev@users.noreply.github.com>
2022-01-29 00:23:09 +0000

    (BROKEN) Spacing checks for tile generator
    Its an overflow which i definitely know how to fix, but im commiting this rn because its half past midnight and id very much like to go get some grub back home xoxoxo

--- Global Games Jam/Assets/Scripts/TilemapGenerator.cs
+++ Global Games Jam/Assets/Scripts/TilemapGenerator.cs
@@ -40,7 +40,23 @@ public class TilemapGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        List<TileType>[] x = new List<TileType>[2];
+        x = GenerateTileRow(10, 2, Time.deltaTime);
+
+        string outputLog = "";
+
+        foreach (TileType y in x[2])
+        {
+            if (y == TileType.Air)
+            {
+                outputLog += "O";
+            } else if (y == TileType.Ground)
+            {
+                outputLog += "X";
+            }
+        }
+
+        Debug.Log(outputLog);
     }
 
     //Width in tiles. Playerwidth in tiles (IN THE SAME SCALE AS WIDTH), height to generate as a float (Any scale you want)
@@ -48,24 +64,43 @@ public class TilemapGenerator : MonoBehaviour
     {
         List<TileType>[] temp = new List<TileType>[2];
         
-        for (int layer = 2; layer > -1; layer--)
+        for (int layer = 0; layer < 2; layer++)
         {
             switch (layer)
             {
+
+
+                //TODO
+                //THIS IS BROKEN
+                //Layer overflows a bit and im not sure how to fix it
+                //Sadge
                 case 0:
 
-                    foreach (TileType x in temp[layer - 1])
+                    //Get a set of values from Proc Noise that represents tiles
+                    foreach (float x in noiseGen.GetNoiseAtHeight(width, heightToGenerate))
                     {
-                        temp[layer].Add(TileType.Air);
+                        //Control ground / air gaps
+                        if (x > 0.75)
+                        {
+                            Debug.Log("layer is currently " + layer);
+                            temp[layer].Add(TileType.Ground);
+                        }
+                        else
+                        {
+                            temp[layer].Add(TileType.Air);
+                        }
                     }
 
+                    temp[layer] = ValidateGaps(temp[layer], playerWidth);
+
                     break;
+
 
                 case 1:
                     
                     //Check previous layer for ground below. Generate a random number between 1 and 20.
                     //If you roll a nat 20, spawn a collectable over the ground that would be there
-                    foreach (TileType x in temp[layer - 1])
+                    foreach (TileType x in temp[layer + 1])
                     {
                         int y = Random.Range(1, 20);
                         if (y == 20) //Roll a 20, get a collectable (if spawnable in current position (there is ground under you))
@@ -87,22 +122,12 @@ public class TilemapGenerator : MonoBehaviour
 
                 case 2:
 
-                    //Get a set of values from Proc Noise that represents tiles
-                    foreach (float x in noiseGen.GetNoiseAtHeight(width, heightToGenerate))
+
+                    for (int i = 0; i < width; i++)
                     {
-                        //Control ground / air gaps
-                        if (x > 0.75)
-                        {
-                            temp[layer].Add(TileType.Ground);
-                        }
-                        else
-                        {
-                            temp[layer].Add(TileType.Air);
-                        }
+                        temp[layer].Add(TileType.Air);
                     }
 
-                    temp[layer] = ValidateGaps(temp[layer], playerWidth);
-                    
                     break;
             }
         }
