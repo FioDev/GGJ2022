fe5972931d6d42d76ddc2e01dc63168af8fdf6e5
Solomon Baarda <50365603+SolomonBaarda@users.noreply.github.com>
2022-01-29 22:54:18 +0000

    Added time survived

--- Global Games Jam/Assets/Scripts/Player.cs
+++ Global Games Jam/Assets/Scripts/Player.cs
@@ -7,12 +7,19 @@ public class Player : MonoBehaviour
     public string EffectsLayerForThisPlayer;
     protected bool isDead = false;
 
+    public float TimeSurvivedSeconds = 0;
+
+    private void Update()
+    {
+        TimeSurvivedSeconds += Time.deltaTime;
+    }
+
     public void KillPlayer()
     {
         if (!isDead)
         {
             isDead = true;
-            Debug.Log($"Killed {transform.tag}");
+            Debug.Log($"{transform.tag} survived for {TimeSurvivedSeconds.ToString("0.0")} seconds");
             GameObject g = Instantiate(BloodSplatter, transform.position, Quaternion.identity, null);
             g.layer = LayerMask.NameToLayer(EffectsLayerForThisPlayer);
 
