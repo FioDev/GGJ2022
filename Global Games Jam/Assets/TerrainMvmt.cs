7c0bfcd37b1e072def13042b1bf4bdcd1b2530f6
Solomon Baarda <50365603+SolomonBaarda@users.noreply.github.com>
2022-01-29 17:03:55 +0000

    a

--- Global Games Jam/Assets/TerrainMvmt.cs
+++ Global Games Jam/Assets/TerrainMvmt.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class TerrainMvmt : MonoBehaviour
 {
-    public float stepLength = 10;
-
+    public float MaximumSpeed = 50;
     public float direction = 1;
+    public float TimeUntilMaxSpeedSeconds = 30;
+    public AnimationCurve SpeedOverTime;
 
     Vector2 cameraMotion;
+    float totalTime = 0;
+
     // Update is called once per frame
     void Update()
     {
-        cameraMotion = new Vector2( 0f, stepLength);
+        totalTime += Time.deltaTime;
+
+        cameraMotion = new Vector2(0f, MaximumSpeed * SpeedOverTime.Evaluate(totalTime / TimeUntilMaxSpeedSeconds));
         transform.Translate(cameraMotion * Time.deltaTime * direction);
     }
 }
