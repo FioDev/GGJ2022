43382c7b08b4591d9ef652dc4a5f42ad384dcbf5
Fraser-Napier <fraser.orr1@gmail.com>
2022-01-29 21:36:04 +0000

    player 1 animated

--- Global Games Jam/Assets/LoganPlayer/Scripts/Player/Movement2D.cs
+++ Global Games Jam/Assets/LoganPlayer/Scripts/Player/Movement2D.cs
@@ -4,75 +4,86 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
 
+
 public class Movement2D : MonoBehaviour
 {
 
-	public CharacterController2D controller;
+    public CharacterController2D controller;
 
-	public float runSpeed = 40f;
+    public float runSpeed = 40.0f;
 
-	private bool moving = false;
-	private float horizontalMove = 0f;
-	private bool jump = false;
-	//private bool crouch = false;
-	//private bool fire = false;
+    private bool moving = false;
+    private float horizontalMove = 0.0f;
+    private bool jump = false;
+    //private bool crouch = false;
+    //private bool fire = false;
 
     public float playerNumber = 1;
 
-	// Update is called once per frame
-	void Update()
-	{
-		//Gets the players horizontal direction
-		horizontalMove = Input.GetAxisRaw("Horizontal"+playerNumber) * runSpeed;
+	public Animator animator;
 
-		if(horizontalMove != 0f)
-		{
-			moving = true;
-		}
-		else
-		{
-			moving = false;
-		}
+    // Update is called once per frame
+    void Update()
+    {
+        //Gets the players horizontal direction
+        horizontalMove = Input.GetAxisRaw("Horizontal" + playerNumber) * runSpeed;
+		animator.SetFloat("Speed",Mathf.Abs(horizontalMove));
+        if (horizontalMove != 0.0f)
+        {
+            moving = true;
+        }
+        else
+        {
+            moving = false;
+        }
 
-		/*
+        /*
 		if(Input.GetButton("Left") && Input.GetButton("Right"))
 		{
 			moving = false;
 		}
 		*/
 
-        if(controller.m_Grounded)
+        if (controller.m_Grounded)
         {
             jump = false;
+			animator.SetBool("IsJumping", false);
             //Gets if the player should jumping
-            if (Input.GetButton("Jump"+playerNumber))
+            if (Input.GetButton("Jump" + playerNumber))
             {
                 jump = true;
+				animator.SetBool("IsJumping",true);
             }
 
         }
-		//Gets if the player should jumping
-		// if (Input.GetButtonDown("Jump"))
-		// {
-		// 	jump = true;
-		// }
-		// else
-		// {
-		// 	jump = false;
-		// }
 
-		/*
+        //animator.SetFloat("Speed",Mathf.Abs(horizontalMove));
+        //Gets if the player should jumping
+        // if (Input.GetButtonDown("Jump"))
+        // {
+        // 	jump = true;
+        // }
+        // else
+        // {
+        // 	jump = false;
+        // }
+
+        /*
 		if(Input.GetButtonDown("Cancel"))
         {
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
 		}
 		*/
 
-	}
+    }
 
-	void FixedUpdate()
+	public void OnLanding()
 	{
-		//Passes all inputs to CharacterController2D
-		controller.Move(horizontalMove * Time.fixedDeltaTime, jump, runSpeed, moving);
+		animator.SetBool("IsJumping", false);
 	}
+    void FixedUpdate()
+    {
+        //Passes all inputs to CharacterController2D
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump, runSpeed, moving);
+    }
 }
