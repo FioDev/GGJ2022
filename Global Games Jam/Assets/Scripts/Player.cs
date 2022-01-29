ca9856bf69ef6d5aebdeecd098c32b5d12dbbd83
Solomon Baarda <50365603+SolomonBaarda@users.noreply.github.com>
2022-01-29 22:49:25 +0000

    Added blood splatter

--- Global Games Jam/Assets/Scripts/Player.cs
+++ Global Games Jam/Assets/Scripts/Player.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
+using UnityEditor;
 
 public class Player : MonoBehaviour
 {
     public GameObject BloodSplatter;
+    public string EffectsLayerForThisPlayer;
+    protected bool isDead = false;
 
     public void KillPlayer()
     {
-        Debug.Log($"Killed {transform.tag}");
+        if (!isDead)
+        {
+            isDead = true;
+            Debug.Log($"Killed {transform.tag}");
+            GameObject g = Instantiate(BloodSplatter, transform.position, Quaternion.identity, null);
+            g.layer = LayerMask.NameToLayer(EffectsLayerForThisPlayer);
 
-        Instantiate(BloodSplatter, transform.position, Quaternion.identity, null);
-        //Destroy(gameObject);
+            Destroy(gameObject);
+        }
     }
 }
