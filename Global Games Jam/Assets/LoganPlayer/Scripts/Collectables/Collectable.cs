03b2d6ba06a33b9c5c445137cb2bd20f172b9d77
Cynichill <logancampbell@live.co.uk>
2022-01-30 12:07:19 +0000

    Collectables/Sending debuffs works now

--- Global Games Jam/Assets/LoganPlayer/Scripts/Collectables/Collectable.cs
+++ Global Games Jam/Assets/LoganPlayer/Scripts/Collectables/Collectable.cs
@@ -9,8 +9,10 @@ public class Collectable : MonoBehaviour
     public int value = 1;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player") //If collided with player..
+        Debug.Log("Player Entered Orb");
+        if(collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2") //If collided with player..
         {
+            Debug.Log("Player Entered Orb");
             manager = collision.gameObject.GetComponent<CollectableManager>(); //Get players collectable manager
             manager.AddOrbs(value); //Add value to player
             Destroy(gameObject, 0);
