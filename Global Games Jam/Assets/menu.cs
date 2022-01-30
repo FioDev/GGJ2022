cdb87ae8b396769470b9775822898d213d008f24
Fraser-Napier <fraser.orr1@gmail.com>
2022-01-30 12:28:58 +0000

    re name the scene to join

--- Global Games Jam/Assets/menu.cs
+++ Global Games Jam/Assets/menu.cs
@@ -6,7 +6,7 @@ public class menu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Solomon");
     }
 
     public void QuitGame()
