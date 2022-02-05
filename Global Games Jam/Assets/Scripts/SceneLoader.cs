5b5ce45de1ca95b3b69297ecb5a07aa7ace4b428
Solomon Baarda <solomon_baarda@outlook.com>
2022-02-05 12:32:35 +0000

    a

--- Global Games Jam/Assets/Scripts/SceneLoader.cs
+++ Global Games Jam/Assets/Scripts/SceneLoader.cs
@@ -8,7 +8,7 @@ public class SceneLoader : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetButtonDown("Cancel"))
+        if(Input.GetButtonDown("Quit"))
         {
             Destroy(GameObject.FindGameObjectWithTag("ScoreKeeper"), 0);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
